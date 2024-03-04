812b72b9b414cb1a483b58386e7cb366e9599fae
Felix Lerner <felix.lerner01@gmail.com>
2024-03-04 15:45:00 -0300

    feat: banner now as background img

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -23,12 +23,13 @@ function Banner({ fetchUrl }) {
     }
     console.log(isObjEmpty(movie))
     return (
+        !isObjEmpty(movie) &&
         <header>
             <div className="bg-auto max-h-screen" style={{ backgroundSize: "cover", backgroundImage: `url(${IMAGE_PATH_BASE_URL}${movie.backdrop_path})`, }} >
                 <div className="ml-20 pt-20">
                     <div className="flex flex-col pl-20 pt-20">
                         <h1 className="text-6xl max-w-60 text-[#fff] pb-5">{movie.title ? movie.title : movie.name}</h1>
-                        <p className="max-w-60  text-[#fff]">{truncateText(movie.overview, 300)}</p>
+                        <p className="max-w-60  text-[#fff]">{truncateText(movie.overview, 200)}</p>
                     </div>
                     <div className="pl-20 pt-10 pb-60">
                         <button className="bg-[#fff] text-black hover:bg-slate-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Play</button>
