7da4880cea9948ace2f73e1f0bf99046ec89a5a3
Felix Lerner <felix.lerner01@gmail.com>
2024-03-05 11:54:38 -0300

    refactor: cleaning code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -30,7 +30,7 @@ function Banner({ fetchUrl }) {
                 <div className="ml-20 pt-20">
                     <div className="flex flex-col pl-5 pt-20">
                         <h1 className="text-6xl max-w-60 text-[#fff] pb-5">{movie.title ? movie.title : movie.name}</h1>
-                        <p className="max-w-60  text-[#fff]">{truncateText(movie.overview, 200)}</p>
+                        <p className="max-w-56  text-[#fff]">{truncateText(movie.overview, 200)}</p>
                     </div>
                     <div className="pl-5 pt-10 pb-40 flex">
                         <button className="bg-[#fff] text-black hover:bg-slate-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex items-center"><FaPlay />Play</button>
