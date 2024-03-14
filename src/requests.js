0cc15077dc6c6b2db2eed4bae6eaadce29a8040c
Felix Lerner <felix.lerner01@gmail.com>
2024-03-14 12:09:22 -0300

    fix: env variables

--- src/requests.js
+++ src/requests.js
@@ -4,43 +4,43 @@ console.log(API_KEY)
 const requests = [
   {
     id: 0,
-    title: "Trending",
+    title: 'Trending',
     endPoint: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
   {
     id: 1,
-    title: "NETFLIX ORIGINALS",
+    title: 'NETFLIX ORIGINALS',
     endPoint: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     isVerticalImage: true,
   },
   {
     id: 2,
-    title: "Top Rated",
+    title: 'Top Rated',
     endPoint: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   },
   {
     id: 3,
-    title: "Action",
+    title: 'Action',
     endPoint: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   },
   {
     id: 4,
-    title: "Comedy",
+    title: 'Comedy',
     endPoint: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   },
   {
     id: 5,
-    title: "Horror",
+    title: 'Horror',
     endPoint: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   },
   {
     id: 6,
-    title: "Romance",
+    title: 'Romance',
     endPoint: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   },
   {
     id: 7,
-    title: "Documentaries",
+    title: 'Documentaries',
     endPoint: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
   },
 ]
